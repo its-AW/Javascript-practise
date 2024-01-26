9adc792cc25a61dfaa3a91f9f9d235df34352849
its-AW <154331664+its-AW@users.noreply.github.com>
2024-01-26 06:31:53 +0000

    datatypes

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,12 @@
 console.log("Jai Shree Ram")
+
+const id = 2124;
+let email = "abc@gmail.com"
+var password = "23657"
+city = "Ambala"
+let state; // undefined
+
+console.table([id, email, password, city]);
+
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
